e128162b360bd038b3925c4274e8d46e39ad4442
Jose Andres <joseandres216@estudiantec.cr>
2021-05-14 23:23:49 -0600

    Fixed bug of main window backgound
    Now the background image is visible

--- main.cpp
+++ main.cpp
@@ -2,9 +2,11 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::RenderWindow window(sf::VideoMode(1600, 900), "Let's Play");
+    sf::Texture background;
+    if (!background.loadFromFile("/home/joseandres216/Escritorio/Jose Andres/TEC/Semestre V/Datos II/Proyecto II - Games/Client/Proyecto_II_Client_CE2103-/content/MenuBackground.png"))
+        return EXIT_FAILURE;
+    sf::Sprite backgroundSprite(background);
 
     while (window.isOpen())
     {
@@ -14,10 +16,23 @@ int main()
             if (event.type == sf::Event::Closed)
                 window.close();
         }
-
         window.clear();
-        window.draw(shape);
+        window.draw(backgroundSprite);
         window.display();
+
+
+        while (window.isOpen())
+        {
+            sf::Event event;
+            while (window.pollEvent(event))
+            {
+                if (event.type == sf::Event::Closed)
+                    window.close();
+            }
+            window.clear();
+            window.draw(backgroundSprite);
+            window.display();
+        }
     }
 
     return 0;
