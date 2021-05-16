60714fc8305fd4a34e2aa0e6a2098f5e9dd078ce
Jose Andres <joseandres216@estudiantec.cr>
2021-05-16 01:05:52 -0600

    Worked on PB Game Settings Window
    Not definitive PB Game Settings window, need some changes

--- Views/BPGameSettings.h
+++ Views/BPGameSettings.h
@@ -1,3 +1,6 @@
+using namespace std;
+#include "iostream"
+
 #ifndef PROYECTO_II_CLIENT_CE2103__BPGAMESETTINGS_H
 #define PROYECTO_II_CLIENT_CE2103__BPGAMESETTINGS_H
 
@@ -5,30 +8,39 @@
 #include "BPGame.h"
 
 class BPGameSettings{
+private:
+    string player1 = "";
+    string player2 = "";
+    int playersPerteam = 0;
+    int goalsTowin = 0;
 public:
     int start(){
         int width = 1600;
         int height = 900;
         sf::RenderWindow window(sf::VideoMode(width, height), "BP Game Settings");
 
+        sf::Texture background;
+        if (!background.loadFromFile(
+                "/home/joseandres216/Escritorio/Jose Andres/TEC/Semestre V/Datos II/Proyecto II - Games/Client/Proyecto_II_Client_CE2103-/content/BPGameSettingsBackground.png"))
+            return EXIT_FAILURE;
+        sf::Sprite backgroundSprite(background);
+
         sf::Font font;
         if (!font.loadFromFile(
-                "/home/joseandres216/Escritorio/Jose Andres/TEC/Semestre V/Datos II/Proyecto II - Games/Client/Proyecto_II_Client_CE2103-/Fonts/Games/Games-Italic.ttf")) {
+                "/home/joseandres216/Escritorio/Jose Andres/TEC/Semestre V/Datos II/Proyecto II - Games/Client/Proyecto_II_Client_CE2103-/Fonts/Ubuntu/Ubuntu-Regular.ttf")) {
             window.close();
         }
-
-        sf::Text text;
-        text.setFont(font);
-        text.setString("BPGame Settings Window");
-        text.setCharacterSize(50);
-        text.setFillColor(sf::Color::Red);
-        text.setOutlineColor(sf::Color::White);
-        text.setOutlineThickness(5);
-        text.setPosition(100, 100);
 
         while (window.isOpen()) {
             sf::Event event;
             while (window.pollEvent(event)) {
+                if (event.type == sf::Event::KeyReleased) {
+                    if (event.key.code == sf::Keyboard::Enter) {
+                        window.close();
+                        BPGame *window = new BPGame();
+                        window->start();
+                    }
+                }
                 if (event.type == sf::Event::KeyReleased) {
                     if (event.key.code == sf::Keyboard::Escape) {
                         window.close();
@@ -36,20 +48,12 @@ public:
                 }
                 if (event.type == sf::Event::Closed)
                     window.close();
-                if (event.type == sf::Event::KeyReleased) {
-                    if (event.key.code == sf::Keyboard::A) {
-                        window.close();
-                        BPGame *window = new BPGame();
-                        window->start();
-                    }
-                }
             }
             window.clear();
-            window.draw(text);
+            window.draw(backgroundSprite);
             window.display();
         }
     }
-private:
 };
 
 #endif //PROYECTO_II_CLIENT_CE2103__BPGAMESETTINGS_H
