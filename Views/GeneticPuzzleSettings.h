d878c0e664b4ef1d2d5879558b83191d061d452d
Jose Andres <joseandres216@estudiantec.cr>
2021-05-21 00:35:55 -0600

    Added new background and started with image loader
    Image loader working with std::cin

--- Views/GeneticPuzzleSettings.h
+++ Views/GeneticPuzzleSettings.h
@@ -7,6 +7,7 @@
 class GeneticPuzzleSettings {
 private:
     string difficulty = "2X2";
+    string imageState = "Waiting for\nimage...";
 public:
     int start() {
         int width = 1600;
@@ -19,6 +20,8 @@ public:
                 "../Resources/GeneticPuzzleSettingsBackground.png"))
             return EXIT_FAILURE;
         sf::Sprite geneticPuzzlesettingsBackgroudsprite(geneticPuzzlesettingsBackground);
+
+        sf::Texture selectedImage;
 
         //Font resource loading
         sf::Font font;
@@ -33,7 +36,15 @@ public:
         selectedDifficulty.setString(this->difficulty);
         selectedDifficulty.setCharacterSize(35);
         selectedDifficulty.setFillColor(sf::Color::White);
-        selectedDifficulty.setPosition(900, 629);
+        selectedDifficulty.setPosition(795, 625);
+
+        //Text of the selected difficulty
+        sf::Text loadImage;
+        loadImage.setFont(font);
+        loadImage.setString(this->imageState);
+        loadImage.setCharacterSize(25);
+        loadImage.setFillColor(sf::Color::White);
+        loadImage.setPosition(1100, 610);
 
         while (window.isOpen()) {
             sf::Event event;
@@ -62,6 +73,17 @@ public:
                                    event.mouseButton.y >= 360 &&
                                    event.mouseButton.y <= 500) { //Button for 5X5 difficulty
                             this->difficulty = "5X5";
+                        } else if (event.mouseButton.x >= 890 && event.mouseButton.x <= 1070 &&
+                                  event.mouseButton.y >= 595 &&
+                                  event.mouseButton.y <= 675) { //Button for image loading
+                            string imagePath;
+                            cin >> imagePath;
+                            if (!selectedImage.loadFromFile(
+                                    imagePath)){
+                                loadImage.setString("Couldn't load\nimage");
+                            }else{
+                                loadImage.setString("Image loaded\nsuccesfully!");
+                            }
                         }
                     }
                 }
@@ -84,6 +106,8 @@ public:
             window.draw(geneticPuzzlesettingsBackgroudsprite);
             selectedDifficulty.setString(this->difficulty);
             window.draw(selectedDifficulty);
+            loadImage.setString(this->imageState);
+            window.draw(loadImage);
             window.display();
         }
     }
